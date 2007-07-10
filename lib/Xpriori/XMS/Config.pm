1856b558ac16250ccb56342975317dac36db885c
川合　孝典 <GCD00051@nifty.ne.jp>
2007-07-10 07:52:16 +0000

    Import of KWITKNR/Xpriori-XMS-0.01 from CPAN.
    gitpan-cpan-distribution: Xpriori-XMS
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KWITKNR/Xpriori-XMS-0.01.tar.gz
    gitpan-cpan-author:       KWITKNR
    gitpan-cpan-maturity:     released

---
+++ lib/Xpriori/XMS/Config.pm
@@ -0,0 +1,17 @@
+#   Configuration file to run Xpriori
+package Xpriori::XMS::Config;
+our %_connect = (
+    METHOD   => 'http',
+    SERVER   => 'localhost',
+    PORT     => '7700',
+);
+our %_cnf = (
+    CHARSET  => 'utf-8',   # 'utf-8' or 'Shift-JIS' or 'EUC-JP' or 'ISO-2022-JP'
+    LANGUAGE => 'en',      # 'en' or 'ja'
+);
+our %_svrCnf = (
+    OSMODULE => 'Win32',   # currently : Win32 or Solaris
+    PASSWORD => 'admin',
+    NEOHOME  => 'C:/Xpriori/neoxml', #Windows
+);
+1;
